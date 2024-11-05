1a09d5eb51d662e007bd42e76e743119140ccaf9
MohanDrey24 <mohantampon@gmail.com>
2024-11-05 18:27:00 +0800

    update assistants route

--- server/routes/assistants/index.post.ts
+++ server/routes/assistants/index.post.ts
@@ -1,32 +1,19 @@
-import OpenAI from "openai";
+import Conversation from "~~/src/classes/conversation";
 
 export default defineEventHandler(async(event) => {
-  const { apiKey, assistantId: assistant_id } = useRuntimeConfig(event);
+  const { apiKey, assistantId } = useRuntimeConfig(event);
 
-  // create an openai client
-  const client = new OpenAI({
-    apiKey,
-  });
+  const conversation = new Conversation(apiKey, assistantId);
 
-  // create thread
-  const thread = await client.beta.threads.create({
-    messages: [
-      {
-        role: 'user',
-        content: '"What is Scala?"',
-      },
-    ],
-  });
-  
-  // create message and retrieve message
-  const run = await client.beta.threads.runs.createAndPoll(thread.id, {
-    assistant_id,
-    additional_instructions: 'Please be professional and avoid giving compliments or positive comments. Write your response in Gitlab Markdown Format.'
-  });
+  const message = 'What is Scala';
+  const additionalInstruction = 'Please be professional and avoid giving compliments or positive comments. Write your response in Gitlab Markdown Format.';
 
-  if (run.status === 'completed') {
-    const messages = await client.beta.threads.messages.list(thread.id);
-    const block = messages.data[0]?.content[0];
-    if (block && block.type === 'text') console.log(block.text.value);
+  const response = await conversation.getResponse(message, additionalInstruction);
+
+  if (response) {
+    console.log(response);
+  } else {
+    console.log("Failed to get response");
   };
-})
+});
+
