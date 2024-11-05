ce14272ac49e0cbe2b11bdc4fbe8e88ee0b4d7d2
Mohan Drey Tampon <mdgtampon@vauldex.com>
2024-11-05 17:24:55 +0800

    update users routes

--- server/routes/users/index.post.ts
+++ server/routes/users/index.post.ts
@@ -7,25 +7,22 @@ export default defineEventHandler(async (event) => {
   const { name, age, email } = await readBody(event) as NewUser;
 
   try {
-
     await db.insert(usersTable).values({ 
       name,
       age,
       email,
     });
 
-    return { message: "Data successfully inserted" }
-
+    return { message: "Data successfully inserted" };
   } catch (error) {
-
     if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
-      return createError({
+      throw createError({
         status: 409,
         statusMessage: "Conflict",
         message: "Email already exists",
-      })
-    }
+      });
+    };
 
-    throw error
+    throw error;
   }
 })
