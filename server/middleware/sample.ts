c54b2e9aebf00a99b19538ef1807e53cca0b325b
Mohan Drey Tampon <mdgtampon@vauldex.com>
2024-11-05 17:57:11 +0800

    remove middleware

--- server/middleware/sample.ts
+++ server/middleware/sample.ts
@@ -1,7 +1,3 @@
 export default defineEventHandler(async(event) => {
-  // throw createError({
-  //   status: 409,
-  //   statusMessage: "Conflict",
-  //   message: "Email already exists",
-  // })
+
 })
